876323b7c1812c58c8c87038e58739b23ff5197a
Rogério Carmo <rogeriocarmo.ti@gmail.com>
2026-01-29 01:03:39 -0300

    Add global exception handling

---
+++ backend/src/main/java/br/gov/mt/seplag/backend/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package br.gov.mt.seplag.backend.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
